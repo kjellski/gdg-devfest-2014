bd982cfe32d09806917293840aef98b7200a7eda
kjellski <otto.kjell@gmail.com>
2014-10-18 20:49:34 +0200

    added the initial generated content: yo angular-fullstack gdg-devfest-2014

---
+++ demo/server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/gdgdevfest2014-dev'
+  },
+
+  seedDB: true
+};
